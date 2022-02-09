6f6492901d329e95076018afb3f524bee80eba9c
Cliff Hammerschmidt <z+gh@hammerschmidt.ca>
2022-02-09 19:00:45 +0000

    refactoring tuid.

--- src/lib/tuid.test.ts
+++ src/lib/tuid.test.ts
@@ -1,16 +1,16 @@
 import * as assert from 'assert';
 import * as sinon from 'sinon';
-import { tuidFactoryCtor } from './tuid';
+import {tuidFactoryCtor, tuid, tuidFormatterType} from './tuid';
 
 describe("tuid", () => {
-  const getSut = () => {
+  const getSut = (formatter: tuidFormatterType | undefined = undefined) => {
     const randomFillSyncStub = sinon.stub();
     const nowMsStub = sinon.stub();
-    const tuidFactory = tuidFactoryCtor(randomFillSyncStub, nowMsStub);
-    return { tuidFactory, randomFillSyncStub, nowMsStub };
+    const tuidFactory = tuidFactoryCtor(randomFillSyncStub, nowMsStub, formatter);
+    return {tuidFactory, randomFillSyncStub, nowMsStub};
   };
-  it("works", () => {
-    const { tuidFactory, randomFillSyncStub, nowMsStub } = getSut();
+  it("works w/ default formatter", () => {
+    const {tuidFactory, randomFillSyncStub, nowMsStub} = getSut();
 
     nowMsStub.returns(0x7770_7771_7772_777en);
     assert.strictEqual("77717772777e00000000000000000000", tuidFactory());
@@ -23,5 +23,33 @@ describe("tuid", () => {
       }
     });
     assert.strictEqual("777177727780ffffffffffffffffffff", tuidFactory());
+  });
+
+  it("works w/ b64u formatter", () => {
+    const {tuidFactory, randomFillSyncStub, nowMsStub} = getSut(tuid.base64urlFormatter);
+
+    nowMsStub.returns(0x7770_7771_7772_777en);
+    assert.strictEqual("d3F3cnd-AAAAAAAAAAAAAA", tuidFactory());
+    assert.strictEqual("d3F3cnd_AAAAAAAAAAAAAA", tuidFactory());
+
+    randomFillSyncStub.callsFake((buffer: Buffer, start: number, count: number) => {
+      const end = start + count;
+      for (let i = start; i < end; ++i) {
+        buffer[i] = 0xff;
+      }
+    });
+    assert.strictEqual("d3F3cneA_____________w", tuidFactory());
+  });
+
+  it("hexToBase64url", () => {
+    assert.strictEqual("d3F3cnd-AAAAAAAAAAAAAA", tuid.hexToBase64url("77717772777e00000000000000000000"));
+    assert.strictEqual("d3F3cnd_AAAAAAAAAAAAAA", tuid.hexToBase64url("77717772777f00000000000000000000"));
+    assert.strictEqual("d3F3cneA_____________w", tuid.hexToBase64url("777177727780ffffffffffffffffffff"));
+  });
+
+  it("base64urlToHex", () => {
+    assert.strictEqual("77717772777e00000000000000000000", tuid.base64urlToHex("d3F3cnd-AAAAAAAAAAAAAA"));
+    assert.strictEqual("77717772777f00000000000000000000", tuid.base64urlToHex("d3F3cnd_AAAAAAAAAAAAAA"));
+    assert.strictEqual("777177727780ffffffffffffffffffff", tuid.base64urlToHex("d3F3cneA_____________w"));
   });
 });
--- src/lib/tuid.ts
+++ src/lib/tuid.ts
@@ -1,6 +1,50 @@
+/*
+// original code
+
+import {randomFillSync} from "crypto";
+export const tuidFactory =  () => {
+  let now = BigInt(+ Date.now());
+  if (now <= lastTime) {
+    now = lastTime + 1n;
+  }
+  lastTime = now;
+  const buffer = Buffer.alloc(18);
+  buffer.writeBigInt64BE(now, 0); // 8 bytes, of which we use 6 (48 bits)
+  randomFillSync(buffer, 8, 10); // 10 bytes (80 bits)
+  return buffer.subarray(2, 18).toString('hex');
+};
+*/
+
+const hexFormatter = (buffer: Buffer) => buffer.toString("hex");
+const base64urlFormatter = (buffer: Buffer) => buffer.toString("base64url");
+const hexToBase64url = (s: string) => Buffer.from(s, "hex").toString("base64url");
+const base64urlToHex = (s: string) => Buffer.from(s, "base64url").toString("hex");
+
+export const tuid = {
+  hexFormatter,
+  base64urlFormatter,
+  hexToBase64url,
+  base64urlToHex,
+};
+
+export type tuidFormatterType = (Buffer) => string;
+/**
+ * Returns a 128bit time prefixed (48 bits) random (80 bits) identifier encoded as hex (by default)
+ * Suitable for distributed system identifier generation.
+ * Hard to guess (sparse), increasing security
+ * Semi-monotonic, avoiding index performance issues seen by purely random identifiers
+ * Ideally stored as a fixed length binary field in the DB. If using a varchar or transmitting via a string
+ * the more compact base64url encoding should be used.
+ *
+ * @param randomFillSync matches https://nodejs.org/api/crypto.html#cryptorandomfillsyncbuffer-offset-size
+ * @param nowMs returns the current milliseconds since unix epoc
+ * @param formatter formats the result buffer into a string; defaults to the hexFormatter
+ */
+
 export const tuidFactoryCtor = (
   randomFillSync: (buffer: Buffer, offset: number, count: number) => void,
   nowMs: () => number,
+  formatter: tuidFormatterType | undefined = undefined,
 ) => {
   let lastTime = 0n;
   return () => {
@@ -12,6 +56,7 @@ export const tuidFactoryCtor = (
     const buffer = Buffer.alloc(18);
     buffer.writeBigInt64BE(now, 0); // 8 bytes, of which we use 6 (48 bits)
     randomFillSync(buffer, 8, 10); // 10 bytes (80 bits)
-    return buffer.subarray(2, 18).toString('hex');
+    return (formatter ?? hexFormatter)(buffer.subarray(2, 18));
   };
 }
+
